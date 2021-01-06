85cda5567e841c9d8caa9cc26b401eba856ff2a3
mmiller <miller.2676@osu.edu>
2021-01-06 15:50:20 -0800

    Update gatsby-ssr.js
    remove key value for js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,7 +3,6 @@ import React from 'react'
 export const onRenderBody = ({ setPostBodyComponents }) => {
   setPostBodyComponents([
     <script
-      key="https://assets.osu.edu/osds/develop/js/osds.min.js"
       src="https://assets.osu.edu/osds/develop/js/osds.min.js"
     />
   ])
