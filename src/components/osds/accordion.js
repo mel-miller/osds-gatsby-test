fb78f3c0b9e5c9690f6d52d84b490236a4f4b5dd
mmiller <miller.2676@osu.edu>
2021-01-06 15:25:53 -0800

    build accordion out to use props

--- src/components/osds/accordion.js
+++ src/components/osds/accordion.js
@@ -1,44 +1,24 @@
 import React from "react"
 
-function Accordion() {
+function Accordion(props) {
   return (
-    <div class="osds-accordion" data-allow-multiple>
-    <h3 class="osds-accordion__heading">
-        <button id="osds-accordion__trigger--1" class="osds-accordion__trigger" aria-controls="osds-accordion__panel--1" aria-expanded="false">
-            <span class="osds-accordion__icon" aria-hidden="true"></span>
-            <span class="osds-accordion__title">
-                Test Title 1
-            </span>
-        </button>
-    </h3>
-    <div id="osds-accordion__panel--1" class="osds-accordion__panel" role="region" aria-labelledby="osds-accordion__trigger--1" hidden>
-        I'm baby authentic mixtape franzen beard tumblr prism.
+    <div className="osds-accordion">
+      {props.items.map(item => (
+        <React.Fragment key={item.id}>
+          <h3 className="osds-accordion__heading">
+            <button id={`osds-accordion__trigger--${item.id}`} className="osds-accordion__trigger" aria-controls={`osds-accordion__panel--${item.id}`} aria-expanded="false">
+              <span className="osds-accordion__icon" aria-hidden="true"></span>
+              <span className="osds-accordion__title">
+                  {item.title}
+              </span>
+            </button>
+          </h3>
+          <div id={`osds-accordion__panel--${item.id}`} className="osds-accordion__panel" role="region" aria-labelledby={`osds-accordion__trigger--${item.id}`} hidden>
+            {item.content}
+          </div>
+        </React.Fragment>
+      ))}
     </div>
-
-    <h3 class="osds-accordion__heading">
-        <button id="osds-accordion__trigger--2" class="osds-accordion__trigger" aria-controls="osds-accordion__panel--2" aria-expanded="false">
-            <span class="osds-accordion__icon" aria-hidden="true"></span>
-            <span class="osds-accordion__title">
-                Test Title 2
-            </span>
-        </button>
-    </h3>
-    <div id="osds-accordion__panel--2" class="osds-accordion__panel" role="region" aria-labelledby="osds-accordion__trigger--2" hidden>
-        Ennui waistcoat chicharrones hot chicken everyday carry four dollar toast.
-    </div>
-
-    <h3 class="osds-accordion__heading">
-        <button id="osds-accordion__trigger--3" class="osds-accordion__trigger" aria-controls="osds-accordion__panel--3" aria-expanded="false">
-            <span class="osds-accordion__icon" aria-hidden="true"></span>
-            <span class="osds-accordion__title">
-                Test Title 3
-            </span>
-        </button>
-    </h3>
-    <div id="osds-accordion__panel--3" class="osds-accordion__panel" role="region" aria-labelledby="osds-accordion__trigger--3" hidden>
-        Activated charcoal helvetica fanny pack meh, butcher chartreuse fingerstache subway tile.
-    </div>
-</div>
   )
 }
 
