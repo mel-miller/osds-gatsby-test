20bd5b4ef542e8fb806eb8a3efc5e7632166db19
mmiller <miller.2676@osu.edu>
2021-01-06 15:48:08 -0800

    add props for accordion heading level

--- src/components/osds/accordion.js
+++ src/components/osds/accordion.js
@@ -1,18 +1,19 @@
 import React from "react"
 
 function Accordion(props) {
+  const Heading = props.headingLevel || "h3";
   return (
     <div className="osds-accordion">
       {props.items.map(item => (
         <React.Fragment key={item.id}>
-          <h3 className="osds-accordion__heading">
+          <Heading className="osds-accordion__heading">
             <button id={`osds-accordion__trigger--${item.id}`} className="osds-accordion__trigger" aria-controls={`osds-accordion__panel--${item.id}`} aria-expanded="false">
               <span className="osds-accordion__icon" aria-hidden="true"></span>
               <span className="osds-accordion__title">
-                  {item.title}
+                {item.title}
               </span>
             </button>
-          </h3>
+          </Heading>
           <div id={`osds-accordion__panel--${item.id}`} className="osds-accordion__panel" role="region" aria-labelledby={`osds-accordion__trigger--${item.id}`} hidden>
             {item.content}
           </div>
