20b8e33190c16e1bdc6aa4ff91023f5843a40f3f
mmiller <miller.2676@osu.edu>
2021-01-05 17:16:13 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Layout from "../components/layout"
 const Home = () => (
   <Layout>
     <p>This demo site is built with <a href="https://www.gatsbyjs.com/" target="_blank" rel="noreferrer" class="osds-link">Gatsby</a> to test the implementation of OSDS components. The stylesheet was added via the CDN and the markup was created via React Components.</p>
-    <p>The following components have example pages:</p>
+    <p>At this time the following components have example pages:</p>
 
     <ul class="osds-list-ul">
       <li class="osds-list-item"><Link class="osds-link" to="/alerts">Alerts</Link></li>
