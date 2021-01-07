da27127321833cc2ea742e30a1374b7f0059a4fd
mmiller <miller.2676@osu.edu>
2021-01-06 16:10:44 -0800

    Update index.js
    add h1

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Layout from "../components/layout"
 
 const Home = () => (
   <Layout>
+    <h1>Home</h1>
     <p>This demo site is built with <a href="https://www.gatsbyjs.com/" target="_blank" rel="noreferrer" class="osds-link">Gatsby</a> to test the implementation of OSDS components. The stylesheet was added via the CDN and the markup was created via React Components.</p>
     <p>At this time the following components have example pages:</p>
 
