d952e02ec84dfbb3c6b54a03a574da68bfee7965
mmiller <miller.2676@osu.edu>
2021-01-06 17:57:16 -0800

    finish accordion example page

--- src/components/osds/accordion.js
+++ src/components/osds/accordion.js
@@ -1,20 +1,29 @@
 import React from "react"
 
 function Accordion(props) {
-  const Heading = props.headingLevel || "h3";
+  const Heading = props.headingLevel || "h3"
   return (
     <div className="osds-accordion">
       {props.items.map(item => (
         <React.Fragment key={item.id}>
           <Heading className="osds-accordion__heading">
-            <button id={`osds-accordion__trigger--${item.id}`} className="osds-accordion__trigger" aria-controls={`osds-accordion__panel--${item.id}`} aria-expanded="false">
+            <button
+              id={`osds-accordion__trigger--${item.id}`}
+              className="osds-accordion__trigger"
+              aria-controls={`osds-accordion__panel--${item.id}`}
+              aria-expanded="false"
+            >
               <span className="osds-accordion__icon" aria-hidden="true"></span>
-              <span className="osds-accordion__title">
-                {item.title}
-              </span>
+              <span className="osds-accordion__title">{item.title}</span>
             </button>
           </Heading>
-          <div id={`osds-accordion__panel--${item.id}`} className="osds-accordion__panel" role="region" aria-labelledby={`osds-accordion__trigger--${item.id}`} hidden>
+          <div
+            id={`osds-accordion__panel--${item.id}`}
+            className="osds-accordion__panel"
+            role="region"
+            aria-labelledby={`osds-accordion__trigger--${item.id}`}
+            hidden
+          >
             {item.content}
           </div>
         </React.Fragment>
